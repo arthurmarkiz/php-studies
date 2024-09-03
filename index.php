8c1efbc17a2ddda816223ea35c8a830f935692f1
Arthur Markiz <tutumarkiz26@gmail.com>
2024-09-03 17:36:30 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Constants
+define("STATUS_PAID", "paid");
+const PI = 3.14159;
+
+// Variables types
+$firstName = "Arthur";
+$completed = true;
+$score = 75;
+$price = 1.99;
+$companies = [1, 2, 3, 4, "A", "B", true, false];
+
+echo $firstName . " Status Order: " . STATUS_PAID;
+
+?>
