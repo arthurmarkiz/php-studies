04aa74059839eaf70e2a952fa878b3e9e6e4d89e
Arthur Markiz <tutumarkiz26@gmail.com>
2024-09-03 22:17:50 -0300

    first day of studies

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 // Constants
 define("STATUS_PAID", "paid");
@@ -9,8 +10,122 @@ $firstName = "Arthur";
 $completed = true;
 $score = 75;
 $price = 1.99;
-$companies = [1, 2, 3, 4, "A", "B", true, false];
 
-echo $firstName . " Status Order: " . STATUS_PAID;
+echo $firstName . " Status Order: " . STATUS_PAID . '<br/>';
+
+// ========================================================
+
+if ($score >= 75)
+    echo "PASS";
+else if ($score >= 90)
+    echo "PASS ULTRA";
+else 
+    echo "FAIL";
+
+// --------------------------------
+
+function sum(int $x, int $y) {
+    return $x + $y;
+}
+echo '<br/>' . sum(2, 3) . '<br/>';
+
+// --------------------------------
+
+echo "While: ";
+$i = 1;
+while ($i <= 10) {
+    if ($i == 10) {
+        echo $i . '<br/>';
+        break;
+    }
+    echo $i++ . ', ';
+}
+
+// --------------------------------
+
+echo "For: ";
+for ($i = 1; $i <= 10; $i++) {
+    if ($i == 10) {
+        echo $i . '<br/>';
+        break;
+    }
+    echo $i . ', ';
+}
+
+// --------------------------------
+
+$languages = ["PHP", "Java", "Python"];
+$languages[] = "C++";
+$length = count($languages);
+
+for ($i = 0; $i < $length; $i++) {
+    if ($i == $length - 1) {
+        echo $languages[$i] . '<br/>';
+        break;
+    }
+    echo $languages[$i] . ', ';
+}
+
+// --------------------------------
+
+foreach($languages as $language) {
+    echo $language . '<br/>';
+}
+
+// --------------------------------
+
+$paymentStatus = "rejected";
+
+switch($paymentStatus) {
+    case "paid":
+        echo "Paid";
+        break;
+
+    case "rejected":
+    case "declined":
+        echo "Payment Declined";
+        break;
+
+    case "pending":
+        echo "Pending Payment";
+        break;
+
+    default:
+        echo "Unknown Payment Status";
+}
+
+// --------------------------------
+echo '<br/>';
+
+$paymentStatusDisplay = match($paymentStatus) {
+    "paid" => "Paid",
+    "rejected" => "Payment Declined",
+    "declined" => "Payment Declined",
+    "pending" => "Pending Payment",
+    default => "Unknown Payment Status",
+};
+
+echo "Match: " . $paymentStatusDisplay;
+echo '<br/>';echo '<br/>';
+
+// --------------------------------
+
+// declare - ticks (will be called after a certain number of functions pass)
+function onTick() {
+    echo "Tick <br/>";
+}
+register_tick_function("onTick");
+declare(ticks=3);
+
+$i = 0;
+$length = 10;
+while ($i < $length) {
+    echo $i++ . "<br/>";
+}
+
+// declare - encoding
+
+
+// declare - strict_types
 
 ?>
